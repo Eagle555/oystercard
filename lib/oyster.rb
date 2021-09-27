b1517e5de4ee84212a9fbf9efe4f64a909b8f2d0
Tania Rosa Machado Santana <taniasantana@Tanias-MacBook-Air.local>
2021-09-27 17:10:47 +0100

    Enable top up functionality

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -4,5 +4,9 @@ class Oyster
   def initialize
     @balance = 0
   end
+  
+  def top_up(money)
+    @balance =+ money
+  end
 
 end
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -5,4 +5,13 @@ describe Oyster do
     card = Oyster.new
     expect(card.balance).to eq(0)
    end 
+   it 'it should respond to #top_up' do
+    card = Oyster.new
+    expect(card).to respond_to(:top_up) 
+   end
+   it 'can #top_up the balance' do
+    card = Oyster.new
+    expect{ card.top_up 1 }.to change{card.balance}.by 1
+   end
 end
+
