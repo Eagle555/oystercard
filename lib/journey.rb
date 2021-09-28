4f6fdb0026d1dc017cb0fe9bc7c6f5e8f3395ccc
Rob <rob.eagleheart@gmail.com>
2021-09-28 16:06:29 +0000

    Step 9 co-authored-by: Tania <tanrosa@gmail.com> co-authored-by: Rob <rob.eagleheart@gmail.com>

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,10 +1,15 @@
 class Journey
-
+    
+  attr_accessor :minimum_balance
+  MINIUM_BALANCE = 1
+  
     def initialize
       @journey = false
+      @minimum_balance = MINIUM_BALANCE
     end
 
-    def touch_in
+    def touch_in(oyster)
+      return "Minimum amount of £#{minimum_balance} required!" if oyster < minimum_balance
       @journey = true
     end
 
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -12,12 +12,15 @@ describe Journey do
     expect(journey).not_to be_in_journey
   end
   it 'can #touch_in' do
-    journey.touch_in
+    journey.touch_in(1)
     expect(journey).to be_in_journey
   end
   it 'can #touch_out' do
-    journey.touch_in
+    journey.touch_in(1)
     journey.touch_out
     expect(journey).not_to be_in_journey
+  end
+  it 'minimum balance of £1 on touch in' do
+    expect(journey.touch_in(0)).to eq("Minimum amount of £#{journey.minimum_balance} required!")
   end
 end
